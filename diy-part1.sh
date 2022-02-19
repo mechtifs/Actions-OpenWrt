b389429687e376526c272494bef6f36f5c44e645
Miya Rin <mechtifs@gmail.com>
2022-02-19 21:45:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git sundaqing https://github.com/sundaqiang/openwrt-packages' >>feeds.conf.default
