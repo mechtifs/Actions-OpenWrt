fefa8cc5bd321986d508dc07d32a7924b64e763c
Miya Rin <mechtifs@gmail.com>
2022-02-19 13:40:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Modify defult language
-sed -i 's/+@LUCI_LANG_zh-cn/d' package/lean/default-settings/Makefile
+sed -i 's/ +@LUCI_LANG_zh-cn//g' package/lean/default-settings/Makefile
 
 # Modify defult theme
 sed -i 's/Bootstrap/Argon/g' package/feeds/luci/luci/Makefile
