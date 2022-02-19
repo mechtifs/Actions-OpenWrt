8bfffc284d9632df617a805e1ecb215061c0a926
Miya Rin <mechtifs@gmail.com>
2022-02-20 00:05:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,10 +13,12 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Modify defult language
-sed -i 's/ +@LUCI_LANG_zh-cn//g' package/lean/default-settings/Makefile
+# Modify default language
+sed -i "s/ +@LUCI_LANG_zh-cn//g" package/lean/default-settings/Makefile
+sed -i "/zh-cn/d" package/lean/default-settings/Makefile
+sed -i "s/'zh-cn'/'en'/g" feeds/luci/luci.mk
 
-# Modify defult theme
+# Modify default theme
 sed -i 's/Bootstrap/Argon/g' package/feeds/luci/luci/Makefile
 sed -i 's/bootstrap/argon/g' package/feeds/luci/luci/Makefile
 
